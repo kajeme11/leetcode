01bac00468b0caab010c55c944fa402eb3f3e3d1
kajeme11 <chenequek@gmail.com>
2024-12-29 23:57:17 -0800

    #46

---
+++ solutions/src/main/java/com/problems/backtrack/Permutations46.java
@@ -0,0 +1,34 @@
+package main.java.com.problems.backtrack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations46 {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+//        Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+        System.out.println(permute(nums));
+    }
+
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(res, new ArrayList<>(), nums);
+        return res;
+    }
+
+    public static void backtrack(List<List<Integer>> res, ArrayList<Integer> temp, int[] nums){
+
+        if(temp.size() == nums.length){
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int number: nums){
+            if(temp.contains(number)){
+                continue;
+            }
+            temp.add(number);
+            backtrack(res ,temp, nums);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
